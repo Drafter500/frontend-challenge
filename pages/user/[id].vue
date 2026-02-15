3561e151e1102049bee883af64b040a5ef79d68f
drafter500 <vzemtsov@yahoo.com>
2026-02-15 13:09:53 +0100

    Adds search filter

--- pages/user/[id].vue
+++ pages/user/[id].vue
@@ -1,25 +1,40 @@
 <template>
   <AwesomeArticle>
     <h1>User Todo's</h1>
-    <div class="filter-group">
-      Status:
-      <label v-for="option in completenessFilterOptions" :key="option.value">
-        <input
-          type="radio"
-          name="todoFilter"
-          :value="option.value"
-          v-model="completenessFilter"
-        />
-        {{ option.label }}
-      </label>
+    <div class="controls-bar">
+      <input
+        type="text"
+        v-model="searchQuery"
+        placeholder="Search todo..."
+        class="search-input"
+      />
+
+      <div>
+        Status:
+        <label v-for="option in completenessFilterOptions" :key="option.value">
+          <input
+            type="radio"
+            name="todoFilter"
+            :value="option.value"
+            v-model="completenessFilter"
+          />
+          {{ option.label }}
+        </label>
+      </div>
     </div>
 
-    <ul>
+    <div v-if="pending" class="loading">Loading...</div>
+
+    <ul v-else>
       <li v-for="todo in todos" :key="todo.id">
         <h4>{{ todo.title }}</h4>
         <p>Status: {{ todo.completed ? 'Completed' : 'Pending' }}</p>
       </li>
     </ul>
+
+    <p v-if="!pending && todos?.length === 0">
+      No results found.
+    </p>
   </AwesomeArticle>
 </template>
 
@@ -32,12 +47,26 @@ const completenessFilterOptions = [
   {label: 'Pending', value: false}
 ];
 
-const completenessFilter = useState('completenessStatus', () => null);
+const completenessFilter = useState('completenessStatus', () => true);
+// TODO: Way to improve: add search debounce
+const searchQuery = ref('');
 
-const { data: todos } = useFetch(() => `/users/${route.params.id}/todos`, {
+const { data: todos, pending } = useFetch(() => `/users/${route.params.id}/todos`, {
   baseURL: 'https://jsonplaceholder.typicode.com',
   query: computed(() => ({
     completed: completenessFilter.value ?? undefined,
+    q: searchQuery.value,
   })),
 });
 </script>
+
+<style scoped>
+  .controls-bar {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .loading {
+    margin-top: 1rem;
+  }
+</style>
